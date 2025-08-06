00ad6554d68a19fd70f091b803a1dd99f3640dfb
Kabuñiang Buhawi (KB) Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-08-06 13:16:27 +0200

    update ft_push_swap.c

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -55,29 +55,95 @@
  *   - stops and does nothing (return to prompt)
  */
 
-int	check_valid_args(char **av)
-{
-	if (av[i][j] = '\0')
-		reut
-}
-
-/*
- * Function that writes `"Error\n"` to STDERR.
- * Frees the allocated stacks.
- * And terminates the program with exit(EXIT_FAILURE)
+/**
+ * - Function that writes `"Error\n"` to STDERR.
+ * - Frees the allocated stacks.
+ * - End: terminates the program with `exit(EXIT_FAILURE)`
  */
 void	error_free_exit(t_stack **stack_a, t_stack **stack_b)
 {
-	if (stack_a && stack_b)
+	if (stack_a && stack_b);
 }
 
-// int argc, char **argv
+/**
+ * - Checks if character is a digit
+ * - `c >= '0' && c <= '9'`
+ */
+int	isdigit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+/**
+ * - Checks if character is a sign
+ * - `c == '+' || c == '-'`
+ */
+int issign(char c)
+{
+	if (c == '+' || c == '-')
+		return (1);
+	return (0);
+}
+
+/**
+ * - Checker that looks for valid integers in an arg
+ * - Each int: skips space ' ', a sign '+' '-' and digits
+ * - Returns 0 if anything else is found
+ */
+int	is_valid_arg(char *arg)
+{
+	size_t	i;
+
+	i = 0;
+	while (arg[i] != '\0')
+	{
+		while (arg[i] == ' ')
+			i++;
+		if (issign(arg[i]))
+			i++;
+		if (!isdigit(arg[i]))
+			return (0);
+		while (isdigit(arg[i]))
+			i++;
+		if (arg[i] && arg[i] != ' ')
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * - Checks for valid arguments in ./push_swap ...
+ * - Will check for ./push_swap "" (return error)
+ * - Will check each argument ac > 2
+ */
+int	are_valid_args(char **av)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 1;
+	j = 0;
+	while (av[i][j] == ' ')
+		j++;
+	if (av[i][j] == '\0')
+		return (0);
+	while (av[i] != NULL)
+	{
+		if (!is_valid_arg(av[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int main(int ac, char **av) {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 
 	if (ac < 2)
 		return (0);
-	else (!check_valid_args(av))
-
+	else if (!are_valid_args(av))
+		error_free_exit(NULL, NULL);
 }
