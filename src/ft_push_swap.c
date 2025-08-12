c67c6f9452e9a0f64d9ea956e4fc598ac1031187
Kabuñiang Buhawi (KB) Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-08-12 11:47:33 +0200

    update push_swap
    make function templates

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -81,7 +81,7 @@ void	error_free_exit(t_stack **stack_a, t_stack **stack_b)
 	}
 	exit(EXIT_FAILURE);
 }
-
+// - Allocates node for a number and pushes into stack_a (protected)
 void	push_alloc_node(t_stack **stack_a, int num)
 {
 	t_stack	*node;
@@ -122,8 +122,7 @@ int	parse_and_push(t_stack **stack_a, char *s, int *j)
 
 /**
  * - Function that fills up stack A
- * - Allocates node for a number (protected)
- * - Protects duplicates & int overflow/underflow
+ * - Uses parsing each arg (multiple and single)
  */
 void	fill_stack_a(t_stack **stack_a, int ac, char **av)
 {
@@ -144,85 +143,100 @@ void	fill_stack_a(t_stack **stack_a, int ac, char **av)
 	}
 }
 
-int main() {
-	char *one_line[] = {"./a.out", "1 2 3 4 5 6 7 8 9 10", NULL};
-	char *mult_line[] = {"./a.out", "-1", "-300", "-500", NULL};
-	printf("one_line: %i\n", are_valid_args(one_line));
-	printf("mult line: %i\n", are_valid_args(mult_line));
+/**
+ * - Checks the stack for duplicates (will exit and free)
+ * - Fills the stack with indexes of the numbers
+ */
+void	check_and_fill(t_stack **stack_a)
+{
 
-	// check stack filler
-	t_stack *stack;
-	stack = NULL;
-	int ac = 0;
-	while (one_line[ac] != NULL)
-		ac++;
-	printf("number of nums (ac) for one line: %i\n", ac);
-
-	printf("Calling fill_stack_a for one line\n");
-	fill_stack_a(&stack, ac, one_line);
-	printf("Returned from fill_stack_a for one line\n");
-
-	t_stack *cursor = stack;
-	int count = 0; // debug for infinite loop
-	while (cursor != NULL)
-	{
-		printf("stack num: %i\n", cursor->number);
-		cursor = cursor->next;
-		count++;
-		if (count > 100) break ;
-	}
-
-	t_stack *stack2 = NULL;
-	int argc = 0;
-	while (mult_line[argc] != NULL)
-		argc++;
-	printf("number of nums in multiple line: %i\n", argc);
-
-	printf("Calling fill_stack_a for mult line\n");
-	fill_stack_a(&stack2, argc, mult_line);
-	printf("Returned from fill_stack_a for mult line\n");
-
-	cursor = stack2;
-	int counter = 0;
-	while (cursor != NULL)
-	{
-		printf("stack mult num: %i\n", cursor->number);
-		cursor = cursor->next;
-		counter++;
-		if (counter > 100) break ;
-	}
-	// free everything
-	// int i = 0;
-	// while (test[i] != NULL)
-	// {
-	// 	free(split[i]);
-	// 	i++;
-	// }
-	// free(split);
-	error_free_exit(&stack, &stack2);
-	return (0);
 }
 
-// int main(int ac, char **av)
-// {
-// 	t_stack	*stack_a;
-// 	t_stack	*stack_b;
-// 	char	**temp;
+/**
+ * - Does the sorting
+ */
+void	do_push_swap(t_stack **stack_a, t_stack **stack_b)
+{
+	return ;
+}
 
-// 	if (ac < 2)
-// 		return (0);
-// 	else if (!are_valid_args(av))
-// 		error_free_exit(NULL, NULL);
-// 	if (ac == 2)
-// 	{
+// int main() {
+// 	char *one_line[] = {"./a.out", "1 2 3 4 5 6 7 8 9 10", NULL};
+// 	char *mult_line[] = {"./a.out", "-1", "-300", "-500", NULL};
+// 	printf("one_line: %i\n", are_valid_args(one_line));
+// 	printf("mult line: %i\n", are_valid_args(mult_line));
 
-// 	}
-// 	str = ft_split(av[1], ' ');
-// 	fill_stack_a(&stack_a, ac, str);
-// 	stack_b = NULL;
-// 	while (cursor->next != NULL)
+// 	// check stack filler
+// 	t_stack *stack;
+// 	stack = NULL;
+// 	int ac = 0;
+// 	while (one_line[ac] != NULL)
+// 		ac++;
+// 	printf("number of nums (ac) for one line: %i\n", ac);
+
+// 	printf("Calling fill_stack_a for one line\n");
+// 	fill_stack_a(&stack, ac, one_line);
+// 	printf("Returned from fill_stack_a for one line\n");
+
+// 	t_stack *cursor = stack;
+// 	int count = 0; // debug for infinite loop
+// 	while (cursor != NULL)
 // 	{
-// 		printf("num: %i", cursor->number);
+// 		printf("stack num: %i\n", cursor->number);
 // 		cursor = cursor->next;
+// 		count++;
+// 		if (count > 100) break ;
 // 	}
+
+// 	t_stack *stack2 = NULL;
+// 	int argc = 0;
+// 	while (mult_line[argc] != NULL)
+// 		argc++;
+// 	printf("number of nums in multiple line: %i\n", argc);
+
+// 	printf("Calling fill_stack_a for mult line\n");
+// 	fill_stack_a(&stack2, argc, mult_line);
+// 	printf("Returned from fill_stack_a for mult line\n");
+
+// 	cursor = stack2;
+// 	int counter = 0;
+// 	while (cursor != NULL)
+// 	{
+// 		printf("stack mult num: %i\n", cursor->number);
+// 		cursor = cursor->next;
+// 		counter++;
+// 		if (counter > 100) break ;
+// 	}
+// 	// free everything
+// 	// int i = 0;
+// 	// while (test[i] != NULL)
+// 	// {
+// 	// 	free(split[i]);
+// 	// 	i++;
+// 	// }
+// 	// free(split);
+// 	error_free_exit(&stack, &stack2);
+// 	return (0);
 // }
+
+int main(int ac, char **av)
+{
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+
+	if (ac < 2)
+		return (0);
+	else if (!are_valid_args(av))
+		error_free_exit(NULL, NULL);
+	fill_stack_a(&stack_a, ac, av);
+	check_and_fill(&stack_a);
+	stack_b = NULL;
+	do_push_swap(&stack_a, &stack_b);
+
+	t_stack *cursor = stack_a;
+	while (cursor != NULL)
+	{
+		printf("num: %i\n", cursor->number);
+		cursor = cursor->next;
+	}
+}
