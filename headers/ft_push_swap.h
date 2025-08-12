1d3461d6cf79566a1d751d6bd8275a86098e4571
Kabuñiang Buhawi (KB) Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-08-12 10:47:33 +0200

    update header

--- headers/ft_push_swap.h
+++ headers/ft_push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_push_swap.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmonjard <kmonjard@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: kmonjard <kmonjard@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 10:56:21 by kmonjard          #+#    #+#             */
-/*   Updated: 2025/08/06 14:29:50 by kmonjard         ###   ########.fr       */
+/*   Updated: 2025/08/06 10:58:13 by kmonjard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,14 @@ typedef struct s_stack
 }					t_stack;
 
 # include <unistd.h>
+
+int	is_valid_arg(char *arg);
+int	are_valid_args(char **av);
+
+t_stack	*stack_new_node(int number);
+void stack_push_bottom(t_stack **stack, t_stack *new_node);
+
+int	ft_isdigit(char c);
+int ft_issign(char c);
 
 #endif
