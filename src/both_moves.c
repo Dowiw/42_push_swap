a37b35af0c82278938219bbc1726fcc1c9048c8a
Kabuñiang Buhawi (KB) Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-08-20 19:16:42 +0200

    add: functions ss, rr, rrr

--- src/both_moves.c
+++ src/both_moves.c
@@ -11,4 +11,37 @@
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
+#include <unistd.h>
 
+void	do_ss(t_stack **stack_a, t_stack **stack_b)
+{
+	if (!stack_b || !*stack_b || !(*stack_b)->next)
+		return ;
+	if (!stack_a || !*stack_a || !(*stack_a)->next)
+		return ;
+	do_sa(stack_a, 0);
+	do_sb(stack_b, 0);
+	write(1, "ss\n", 3);
+}
+
+void	do_rr(t_stack **stack_a, t_stack **stack_b)
+{
+	if (!stack_b || !*stack_b || !(*stack_b)->next)
+		return ;
+	if (!stack_a || !*stack_a || !(*stack_a)->next)
+		return ;
+	do_ra(stack_a, 0);
+	do_rb(stack_b, 0);
+	write(1, "rr\n", 3);
+}
+
+void	do_rrr(t_stack **stack_a, t_stack **stack_b)
+{
+	if (!stack_b || !*stack_b || !(*stack_b)->next)
+		return ;
+	if (!stack_a || !*stack_a || !(*stack_a)->next)
+		return ;
+	do_rra(stack_a, 0);
+	do_rrb(stack_b, 0);
+	write(1, "rrr\n", 3);
+}
