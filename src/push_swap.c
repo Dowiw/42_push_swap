dde901cd77436a9a8f0d10aa059e7aca8c5c2e4e
Kabuñiang Buhawi (KB) Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-08-20 19:21:50 +0200

    rm: large_sort radix, too much moves. TODO
    will be doing cost-based, something like mechanical turk or another algorithm, back to the drawing board

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,45 +6,23 @@
 /*   By: kmonjard <kmonjard@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 17:15:18 by kmonjard          #+#    #+#             */
-/*   Updated: 2025/08/17 17:15:21 by kmonjard         ###   ########.fr       */
+/*   Updated: 2025/08/20 16:37:25 by kmonjard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 #include <unistd.h>
+#include <limits.h>
 
 /**
  * - Does sorting for more than 5 size
  * - Contains radix as main logic
  */
-void	large_sort(t_stack **stack_a, t_stack **stack_b, int size)
+void	large_sort(t_stack **a, t_stack **b, int size)
 {
-	int	max_bits;
-	int	i;
-	int	j;
-	int	num;
 
-	max_bits = 0;
-	while ((size - 1) >> max_bits)
-		max_bits++;
-	i = 0;
-	while (i < max_bits)
-	{
-		j = 0;
-		while (j < size)
-		{
-			num = (*stack_a)->correct_idx;
-			if (((num >> i) & 1) == 0)
-				do_pb(stack_a, stack_b);
-			else
-				do_ra(stack_a);
-			j++;
-		}
-		while (*stack_b)
-			do_pa(stack_a, stack_b);
-		i++;
-	}
 }
+
 
 int	is_sorted(t_stack **stack_a)
 {
@@ -72,8 +50,8 @@ void	do_push_swap(t_stack **stack_a, t_stack **stack_b)
 		return ;
 	size = get_stack_size(stack_a);
 	cursor = *stack_a;
-	if (size == 2 && cursor->correct_idx < cursor->next->correct_idx)
-		do_sa(stack_a);
+	if (size == 2 && cursor->correct_idx > cursor->next->correct_idx)
+		do_sa(stack_a, 1);
 	else if (size == 3)
 		sort_three(stack_a);
 	else if (size == 4)
