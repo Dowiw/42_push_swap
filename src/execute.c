e55ff1883e6a7f4e1e59f4f60f9068261922404b
Dowiw <monjie12272@gmail.com>
2025-09-02 01:07:57 +0200

    mv: optimize to execute.c

--- src/execute.c
+++ src/execute.c
@@ -1,6 +1,10 @@
 #include "ft_push_swap.h"
 #include <string.h>
 
+/**
+ * - Recursively check if the numbers above and below the current moves
+ * 	have ra and rb, and etc. for other combined moves
+ */
 void	recursive_rr(t_moves *moves, unsigned int i, unsigned int total)
 {
 	if (i + 1 >= total)
@@ -41,36 +45,45 @@ void	recursive_rr(t_moves *moves, unsigned int i, unsigned int total)
 		recursive_rr(moves, i + 1, total);
 }
 
+/**
+ * - Container for optimizations before execution
+ */
 void	optimize_moves(t_moves *moves)
 {
 	recursive_rr(moves, 0, moves->total_moves);
 }
 
-void	perform_move(char *move, t_stack **a, t_stack **b)
+/**
+ * - Perform the move based on the enum given
+ */
+void	perform_move(t_actions move, t_stack **a, t_stack **b)
 {
-	if (strcmp(move, "ra") == 0)
+	if (move == RA)
 		do_ra(a, 1);
-	else if (strcmp(move, "rra") == 0)
+	else if (move == RRA)
 		do_rra(a, 1);
-	else if (strcmp(move, "rb") == 0)
+	else if (move == RB)
 		do_rb(b, 1);
-	else if (strcmp(move, "rrb") == 0)
+	else if (move == RRB)
 		do_rrb(b, 1);
-	else if (strcmp(move, "rr") == 0)
+	else if (move == RR)
 		do_rr(a, b);
-	else if (strcmp(move, "rrr") == 0)
+	else if (move == RRR)
 		do_rrr(a, b);
-	else if (strcmp(move, "pb") == 0)
+	else if (move == PB)
 		do_pb(a, b);
-	else if (strcmp(move, "pa") == 0)
+	else if (move == PA)
 		do_pa(b, a);
-	else if (strcmp(move, "sa") == 0)
+	else if (move == SA)
 		do_sa(a, 1);
-	else if (strcmp(move, "sb") == 0)
+	else if (move == SB)
 		do_sb(b, 1);
+	else if (move == SS)
+		do_ss(a, b);
 }
 /**
  * - Execute moves minimally
+ * - Do optimizations before performing
  */
 void	execute_moves(t_moves *moves, t_stack **a, t_stack **b)
 {
