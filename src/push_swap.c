f8699dfa9bc3446c9723e151a99d1e57066a1fb4
Dowiw <monjie12272@gmail.com>
2025-09-02 01:12:26 +0200

    add: main logic

--- src/push_swap.c
+++ src/push_swap.c
@@ -17,225 +17,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	is_sorted(t_stack **a);
-
-void	free_data(t_data *data)
-{
-	if (!data)
-		return;
-	if (data->chunks)
-		free(data->chunks);
-	if (data->chunk_sizes)
-		free(data->chunk_sizes);
-	free(data);
-}
-
-t_stack *get_bottom_node(t_stack **b, int *pos)
-{
-	t_stack *current = *b;
-	int i = 0;
-	while (current && current->next)
-	{
-		current = current->next;
-		i++;
-	}
-	if (pos)
-		*pos = i;
-	return (current);
-}
-
-// Add free_moves implementation
-void free_moves(t_moves *moves)
-{
-	if (!moves)
-		return;
-	if (moves->seq)
-	{
-		for (unsigned int i = 0; moves->seq[i]; i++)
-			free(moves->seq[i]);
-		free(moves->seq);
-	}
-	free(moves);
-}
-
-/**
- * - Chunk count for fine tuning
- * - Size represents the size itself
- * - Should be remembered that it is NOT **0-indexed**
- */
-unsigned int	calc_chunk_count(unsigned int size)
-{
-	if (size <= 10)
-		return (3);
-	else if (size <= 50)
-		return (3);
-	else if (size <= 100)
-		return (3);
-	else if (size <= 200)
-		return (4);
-	else if (size <= 500)
-		return (14);
-	else
-		return (15);
-}
-
-/**
- * - Initialize chunk struct values
- * - Chunks for chunks array, respective sizes,
- *  the number of chunks, and stack a for min max
- */
-void	init_chunks(t_chunk *chunks, unsigned int *sizes, int count)
-{
-	int	i;
-	int	current_min;
-
-	i = 0;
-	current_min = 0;
-	while (i < count)
-	{
-		chunks[i].chunk_id = i;
-		chunks[i].min_value = current_min;
-		chunks[i].max_value = current_min + sizes[i] - 1;
-		chunks[i].half = sizes[i] / 2;
-		chunks[i].size = sizes[i];
-		chunks[i].current_count = 0;
-		current_min += sizes[i];
-		i++;
-	}
-}
-
-/**
- * - Assign each chunk size based on the total size and chunk count
- * - It does this in a decreasing manner where the first is big
- * - This means that the small numbers have a big size
- */
-void	init_chunk_sizes(unsigned int *chunk_sizes, int size, int chunk_count)
-{
-	int	weight;
-	int	assigned;
-	int	total_weight;
-	int	i;
-
-	total_weight = 0;
-	for (i = 0; i < chunk_count; i++)
-		total_weight += (chunk_count - i - 1);
-	assigned = 0;
-	for (i = 0; i < chunk_count; i++)
-	{
-		weight = chunk_count - i - 1;
-		if (i == chunk_count - 1)
-			chunk_sizes[i] = size - assigned;
-		else
-		{
-			chunk_sizes[i] = (size * weight) / total_weight;
-			assigned += chunk_sizes[i];
-		}
-	}
-}
-
-/**
- * - Assign each i which is correct_idx their chunk id
- * - Compares to each chunks min and max
- */
-unsigned int	assign_id(unsigned int correct_i, t_data *data)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (i < data->num_chunks)
-	{
-		if (correct_i >= data->chunks[i].min_value &&
-			correct_i <= data->chunks[i].max_value)
-		{
-			data->chunks[i].current_count++;
-			return (i);
-		}
-		i++;
-	}
-	write(1, "Error in assign id\n", 20);
-	return (i);
-}
-
-/**
- * - Assign each stack_a their proper chunk_id
- */
-void	assign_stack_a(t_data *data, t_stack **a)
-{
-	unsigned int	correct_i;
-	t_stack			*current;
-
-	current = *a;
-	while (current)
-	{
-		correct_i = current->correct_idx;
-		current->chunk_id = assign_id(correct_i, data);
-		current = current->next;
-	}
-}
-
-/**
- * - Initialize data and assign each node in a its stack id
- * - Initializes each chunk with their respective sizes and min/max
- * - Malloc protects from data
- */
-void	init_data_and_assign(t_stack **a, int size, t_data *data)
-{
-	unsigned int	chunk_count;
-
-	chunk_count = calc_chunk_count(size);
-	// printf("Number of chunks: %u\n", chunk_count);
-	data->chunks = malloc(sizeof(t_chunk) * chunk_count);
-	if (!data->chunks)
-	{
-		free_data(data);
-		error_free_exit(a, NULL);
-	}
-	data->chunk_sizes = malloc(sizeof(unsigned int) * chunk_count);
-	if (!data->chunk_sizes)
-	{
-		free_data(data);
-		error_free_exit(a, NULL);
-	}
-	init_chunk_sizes(data->chunk_sizes, size, chunk_count);
-	data->num_chunks = chunk_count;
-	data->total_size = size;
-	init_chunks(data->chunks, data->chunk_sizes, chunk_count);
-	assign_stack_a(data, a);
-}
-
-t_moves	*init_moves(int size)
-{
-	int		i = 0;
-	t_moves	*moves;
-
-	moves = malloc(sizeof(t_moves));
-	if (!moves)
-		return (NULL);
-	moves->seq = malloc(sizeof(char *) * (size + 1));
-	if (!moves->seq)
-	{
-		free(moves);
-		return (NULL);
-	}
-	moves->seq[size] = NULL;
-	while (i < size)
-	{
-		moves->seq[i] = malloc(sizeof(char) * 4);
-		if (!moves->seq[i])
-		{
-			while (--i >= 0)
-				free(moves->seq[i]);
-			free(moves->seq);
-			free(moves);
-			return (NULL);
-		}
-		moves->seq[i][0] = '\0';
-		i++;
-	}
-	moves->total_moves = INT_MAX;
-	return (moves);
-}
-
 int find_target_pos_a(t_stack **a, int value)
 {
 	t_stack *current = *a;
@@ -318,90 +99,54 @@ void push_back_to_a(t_stack **a, t_stack **b)
 {
 	while (get_stack_size(b) > 0)
 	{
-		// 1. Find largest chunk_id in B
-		int max_chunk_id = -1;
-		int pos = 0, pos_top = -1, pos_bottom = -1;
-		t_stack *cur = *b;
-		int size_b = get_stack_size(b);
-		while (cur)
-		{
-			if ((int)cur->chunk_id > max_chunk_id)
-				max_chunk_id = cur->chunk_id;
-			cur = cur->next;
-		}
-
-		// 2. Find top and bottom node with max_chunk_id
-		t_stack *top_node = NULL, *bottom_node = NULL;
-		cur = *b;
-		pos = 0;
-		while (cur)
-		{
-			if ((int)cur->chunk_id == max_chunk_id)
-			{
-				if (!top_node)
-				{
-					top_node = cur;
-					pos_top = pos;
-				}
-				bottom_node = cur;
-				pos_bottom = pos;
-			}
-			cur = cur->next;
-			pos++;
-		}
-
-		// 3. Calculate moves for top_node
-		int target_pos_a_top = find_target_pos_a(a, top_node->number);
+		int min_total_moves = INT_MAX;
+		int best_moves_a = 0, best_moves_b = 0, best_common = 0;
 		int size_a = get_stack_size(a);
-		int moves_a_top = (target_pos_a_top <= size_a / 2) ? target_pos_a_top : target_pos_a_top - size_a;
-		int moves_b_top = (pos_top <= size_b / 2) ? pos_top : pos_top - size_b;
-		int common_top = 0;
-		if (moves_a_top > 0 && moves_b_top > 0)
-			common_top = (moves_a_top < moves_b_top) ? moves_a_top : moves_b_top;
-		else if (moves_a_top < 0 && moves_b_top < 0)
-			common_top = (moves_a_top > moves_b_top) ? moves_a_top : moves_b_top;
-		int total_moves_top = abs(moves_a_top) + abs(moves_b_top) - abs(common_top);
+		int size_b = get_stack_size(b);
+		int pos = 0;
 
-		// 4. Calculate moves for bottom_node
-		int target_pos_a_bottom = find_target_pos_a(a, bottom_node->number);
-		int moves_a_bottom = (target_pos_a_bottom <= size_a / 2) ? target_pos_a_bottom : target_pos_a_bottom - size_a;
-		int moves_b_bottom = (pos_bottom <= size_b / 2) ? pos_bottom : pos_bottom - size_b;
-		int common_bottom = 0;
-		if (moves_a_bottom > 0 && moves_b_bottom > 0)
-			common_bottom = (moves_a_bottom < moves_b_bottom) ? moves_a_bottom : moves_b_bottom;
-		else if (moves_a_bottom < 0 && moves_b_bottom < 0)
-			common_bottom = (moves_a_bottom > moves_b_bottom) ? moves_a_bottom : moves_b_bottom;
-		int total_moves_bottom = abs(moves_a_bottom) + abs(moves_b_bottom) - abs(common_bottom);
-
-		// 5. Choose best option
-		int use_top = (total_moves_top <= total_moves_bottom);
-
-		// 6. Execute moves
-		int moves_a = use_top ? moves_a_top : moves_a_bottom;
-		int moves_b = use_top ? moves_b_top : moves_b_bottom;
-		int common = use_top ? common_top : common_bottom;
+		// super greedy, get every single move for every single b
+		for (t_stack *cur = *b; cur; cur = cur->next, pos++)
+		{
+			int target_pos_a = find_target_pos_a(a, cur->number);
+			int moves_a = (target_pos_a <= size_a / 2) ? target_pos_a : target_pos_a - size_a;
+			int moves_b = (pos <= size_b / 2) ? pos : pos - size_b;
+			int common = 0;
+			if (moves_a > 0 && moves_b > 0)
+				common = (moves_a < moves_b) ? moves_a : moves_b;
+			else if (moves_a < 0 && moves_b < 0)
+				common = (moves_a > moves_b) ? moves_a : moves_b;
+			int total_moves = abs(moves_a) + abs(moves_b) - abs(common);
+			if (total_moves < min_total_moves)
+			{
+				min_total_moves = total_moves;
+				best_moves_a = moves_a;
+				best_moves_b = moves_b;
+				best_common = common;
+			}
+		}
 
 		// Perform common rotations
-		while (common > 0)
+		while (best_common > 0)
 		{
 			do_rr(a, b);
-			moves_a--;
-			moves_b--;
-			common--;
+			best_moves_a--;
+			best_moves_b--;
+			best_common--;
 		}
-		while (common < 0)
+		while (best_common < 0)
 		{
 			do_rrr(a, b);
-			moves_a++;
-			moves_b++;
-			common++;
+			best_moves_a++;
+			best_moves_b++;
+			best_common++;
 		}
 		// Finish remaining rotations for A
-		while (moves_a > 0) { do_ra(a, 1); moves_a--; }
-		while (moves_a < 0) { do_rra(a, 1); moves_a++; }
+		while (best_moves_a > 0) { do_ra(a, 1); best_moves_a--; }
+		while (best_moves_a < 0) { do_rra(a, 1); best_moves_a++; }
 		// Finish remaining rotations for B
-		while (moves_b > 0) { do_rb(b, 1); moves_b--; }
-		while (moves_b < 0) { do_rrb(b, 1); moves_b++; }
+		while (best_moves_b > 0) { do_rb(b, 1); best_moves_b--; }
+		while (best_moves_b < 0) { do_rrb(b, 1); best_moves_b++; }
 
 		// Push from B to A
 		do_pa(b, a);
@@ -443,44 +188,13 @@ int	check_opposite(t_stack *a)
 }
 
 /**
- * - Get moves of the closest node in chunk_i from the bottom or top
- * - Pick which one is best in a way that puts it into stack b where one
- *  half of the chunk is on top and the other on the bottom
- */
-void best_move(t_stack **a, t_stack **b, int range)
-{
-	unsigned int		i;
-
-	i = 0;
-	while (get_stack_size(a) > 0)
-	{
-		if ((*a)->correct_idx >= i && (*a)->correct_idx <= i + range)
-		{
-			do_pb(a, b);
-			if ((*a)->correct_idx > i + range / 2)
-				do_rb(b, 1);// push to bottom half
-			i++;
-		}
-		else if (check_opposite(*a))
-			do_rra(a, 1);
-		else
-			do_ra(a, 1);
-	}
-}
-
-/**
  * - Heart
  */
-void	large_sort(t_stack **a, t_stack **b, int size)
+void	large_sort(t_stack **a, t_stack **b)
 {
 	unsigned int	i = 0;
 	int	range = 37;
-	t_data			*data;
 
-	data = malloc(sizeof(t_data));
-	if (!data)
-		error_free_exit(a, b);
-	init_data_and_assign(a, size, data);
 	while (get_stack_size(a) > 1)
 	{
 		if ((*a)->correct_idx <= i)
@@ -499,25 +213,7 @@ void	large_sort(t_stack **a, t_stack **b, int size)
 		else
 			do_ra(a, 1); // about 2200
 	}
-	push_back_to_a(a, b);
-	free_data(data);
-}
-
-/**
- * - Checks if stack is sorted
- */
-int	is_sorted(t_stack **a)
-{
-	t_stack	*cursor;
-
-	cursor = *a;
-	while (cursor != NULL && cursor->next != NULL)
-	{
-		if (cursor->correct_idx > cursor->next->correct_idx)
-			return (0);
-		cursor = cursor->next;
-	}
-	return (1);
+	push_back_to_a(a, b); // should also be the same
 }
 
 /**
@@ -541,5 +237,5 @@ void	do_push_swap(t_stack **a, t_stack **b)
 	else if (size == 5)
 		sort_five(a, b);
 	else
-		large_sort(a, b, get_stack_size(a));
+		large_sort(a, b);
 }
